d13849c4bf5faccd318c71ba60d89718d5e1f0fe
sha-lin <shalin.rono@student.moringaschool.com>
2021-12-02 10:33:18 +0300

    craete methods for the hosting

--- host.py
+++ host.py
@@ -3,18 +3,19 @@ import sys
 import time
 
 s=socket.socket()
-host=input(str("Please enter host name:"))
-port=1234
 
-try:
-    s.connect((host,port))
-    print("connected to server")
-except:
-    print("connection to server is failed : (")
+host=socket.gethostname()
+print("Server will start on host:",host)
+port=1234
+s.bind((host,port))
+print("Server is bound successfully")
+s.listen(1)
+conn,addr=s.accept()
+print(addr,"has connected")
 while 1:
-   incoming_message=s.recv(1024)
-   incoming_message=incoming_message.decode()
-   print("Server:>>",incoming_message)
-   message=input(str("You:>>"))
-   message=message.encode()
-   s.send(message)
+    message=input(str("You:>>"))
+    message=message.encode()
+    conn.send(message)
+    incoming_message=conn.recv(1024)
+    incoming_message=incoming_message.decode()
+    print("Client:>>",incoming_message)
